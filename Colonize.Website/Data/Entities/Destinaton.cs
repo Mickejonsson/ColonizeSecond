b215c26c841371f2d58814a12dd346345de99de3
Micke <tm.mikaeljonsson@gmail.com>
2020-01-24 14:04:50 +0100

    Add seed data

--- Colonize.Website/Data/Entities/Destinaton.cs
+++ Colonize.Website/Data/Entities/Destinaton.cs
@@ -7,9 +7,22 @@ namespace Colonize.Website.Data.Entities
 {
     public class Destinaton
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public Uri ImageUrl { get; set; }
+        public Destinaton(string name, string description, Uri imageUrl)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
+
+        public Destinaton(int id, string name, string description, Uri imageUrl)
+              : this(name, description, imageUrl)
+        {
+            Id = id;
+        }
+
+        public int Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        public Uri ImageUrl { get; protected set; }
     }
 }
--- Colonize.Website/Pages/Index.cshtml.cs
+++ Colonize.Website/Pages/Index.cshtml.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Colonize.Website.Data.Entities;
-using Microsoft.AspNetCore.Mvc;
+﻿using Colonize.Website.Data.Entities;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 
 namespace Colonize.Website.Pages
 {
     public class IndexModel : PageModel
-   
+
     {
         private readonly ILogger<IndexModel> _logger;
+
         public IList<Voyage> VoyageList = new List<Voyage>();
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
         public void OnGet()
         {
+            
         }
     }
 }
